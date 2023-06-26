cd01c770065dfeb22f258c7bfb0770f46687cad6
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-06-26 19:58:18 -0300

    Finalização navBar

--- src/app/components/MyProfilePic.tsx
+++ src/app/components/MyProfilePic.tsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 
 export default function MyProfilePic() {
   return (
-    <section className="w-full mx-auto">
-      <Image src='/images/82097583.jpg' alt="Lucas" width={200} height={200} priority={true} />
+    <section className="w-full mx-auto p-4">
+      <Image className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto" src='/images/82097583.jpg' alt="Lucas" width={200} height={200} priority={true} />
     </section>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import Image from 'next/image'
-import MyProfilePic from './components/MyProfilePic'
 
 export default function Home() {
   return (
     <main className='p-6 mx-auto'>
       <p className="mt12 mb12 text-3xl text-center dark:text-white">
-        Olá seja bem-vindo👋{' '}
+        Hello and Welcome👋{' '}
         <span className="whitespace-nowrap">
           I am <span className='font-bold'>Lucas</span>
-          <MyProfilePic></MyProfilePic>
         </span>
       </p>
     </main>
